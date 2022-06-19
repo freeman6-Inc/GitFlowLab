267d8d9bb8e4967b5bac277caedf53e3c25322f8
freeman6 <freeman6@gmail.com>
2022-06-19 15:01:44 +0800

    fix float Divide0 has error

--- Lab001/Calculator.cs
+++ Lab001/Calculator.cs
@@ -12,7 +12,7 @@ namespace Lab001
         public int Subtract(int x, int y) => x - y;
         public int Multiply(int x, int y) => x * y;
         public int Divide(int x, int y) => (y==0)? 0: x / y;
-        public float Divide(float x, float y) => x / y;
+        public float Divide(float x, float y) => (y == 0) ? 0 : x / y;
         public float Multiply(float x, float y) => x * y;
     }
 }
